0ab4a38a8b22b1f25a8aa0771f2f7c22ea6462c2
Debra Venckus <debra@venckus.com>
2017-08-23 10:46:01 -0500

    IT-4 update init.d script for bigredbutton

--- src/app/bigredbutton/sites.py
+++ src/app/bigredbutton/sites.py
@@ -10,10 +10,9 @@ class SitesList(object):
 
   list = {
     '0': { 'name': mdash + " Select Site " + mdash },
-    'all': { 'name': "ALL [ah, oh, ph, sh, sp, vf, vh]" },
+    'all': { 'name': "ALL [ah, ph, sh, sp, vf, vh]" },
     mdash: { 'name': mdash, 'attributes': 'role="separator" class="divider"' },
     'ah': { 'name': "Arthritis-health", 'attributes': 'class="site"' },
-    'oh': { 'name': "Osteoporosis-health", 'attributes': 'class="site"' },
     'ph': { 'name': "Pain-health", 'attributes': 'class="site"' },
     'sh': { 'name': "Spine-health", 'attributes': 'class="site"' },
     'sp': { 'name': "Sports-health", 'attributes': 'class="site"' },
@@ -22,8 +21,8 @@ class SitesList(object):
   }
 
   list_order = {
-     'pre-prod': ['0', 'all', mdash, 'ah', 'oh', 'ph', 'sh', 'sp', 'vh', 'vf'],
-     'production': ['0', 'ah', 'oh', 'ph', 'sh', 'sp', 'vh', 'vf']
+     'pre-prod': ['0', 'all', mdash, 'ah', 'ph', 'sh', 'sp', 'vh', 'vf'],
+     'production': ['0', 'ah', 'ph', 'sh', 'sp', 'vh', 'vf']
   }
 
   @staticmethod
