bf5b8de1cac3e3fc597f745d2f26adc6ee63a996
Debra Venckus <debra@venckus.com>
2019-10-09 12:16:52 -0500

    NONE include veritas-core in list of repositories with code of 'vc' and change repo code for veritas-health-config to 'vcfg'

--- src/app/bigredbutton/repositories.py
+++ src/app/bigredbutton/repositories.py
@@ -19,10 +19,11 @@ class Repositories(object):
       'sp': { 'name': "Sports-health", 'attributes': 'class="repo"' },
       'vh': { 'name': "Veritashealth", 'attributes': 'class="repo"'},
       'vf': { 'name': 'Forums', 'attributes': 'class="repo"' },
-      'vc': { 'name': 'Veritas Health Config', 'attributes': 'class="repo"' },
+      'vc': { 'name': 'Veritas Core', 'attributes': 'class="repo"' },
+      'vcfg': { 'name': 'Veritas Health Config', 'attributes': 'class="repo"' },
       'brb': { 'name': 'BigRedButton', 'attributes': 'class="repo"' }
     },
-    'list_order': ['0', 'all', 'h+p', 'pr', 'ah', 'ph', 'sh', 'sp', 'vh', 'vf', 'vc', 'brb']
+    'list_order': ['0', 'all', 'h+p', 'pr', 'ah', 'ph', 'sh', 'sp', 'vh', 'vf', 'vc', 'vcfg', 'brb']
   }
 
   @staticmethod
