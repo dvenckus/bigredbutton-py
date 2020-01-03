b7a891f0268f4fa9d0577d71b8065312a63f78be
Debra Venckus <debra@venckus.com>
2020-01-03 13:18:16 -0600

    NONE switch release script tag checking to the DEV branch

--- src/app/bigredbutton/releases.py
+++ src/app/bigredbutton/releases.py
@@ -24,12 +24,14 @@ class Releases(object):
     # D7 release versions are based on the veritas-profile-d7 repo
     # use the tags from master branch
     # pull the scripts from develop branch
-    #chdir(constants.VH_CONFIG_DEV_REPO)
+    chdir(constants.VH_CONFIG_DEV_REPO)
     #cmd = "/usr/bin/git describe --tags `git rev-list --tags --max-count=1`"
-    repo = git.Repo(constants.VH_CONFIG_MASTER_REPO)
+    
+    #repo = git.Repo(constants.VH_CONFIG_MASTER_REPO)
+    repo = git.Repo(constants.VH_CONFIG_DEV_REPO)
     last_tag = repo.git.describe('--tags', '--candidates=1')
     last_tag = last_tag[0:7]
-    app.logger.info("Repo: {}".format(constants.VH_CONFIG_MASTER_REPO))
+    app.logger.info("Repo: {}".format(constants.VH_CONFIG_DEV_REPO))
     app.logger.info("Lastag: {}".format(last_tag))
 
     chdir(constants.RELEASE_SCRIPTS_DIR)
